9d3bdb134474e01dca77c20c6f800c5a467ad6ef
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-07-22 23:55:50 +0530

    Moved into controllers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { readdirSync } from 'fs';
+//Load all environment variables
+require('dotenv').config();
 
 const app = express();
 
@@ -8,6 +11,7 @@ readdirSync('./routes').map((route) => {
   app.use('/api', require(`./routes/${route}`));
 });
 
+const port = process.env.PORT || 8000;
 app.listen(8000, () => {
   console.log(`Listening on port 8000 ✅`);
 });
